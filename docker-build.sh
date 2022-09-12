94a23906b2a8c40b54f237bf7c185bb32a81459e
Quan Nguyen-Le-Minh <fx.nguyenleminhquan@gmail.com>
2022-09-12 15:23:06 +0700

    chore: Add pass parameters UNITY_USERNAME & UNITY_PASSWORD to docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -7,8 +7,8 @@ docker run -it --rm \
   -v $(pwd):/project/ \
   -e "BUILD_NAME=GameCI" \
   -e "BUILD_TARGET=Android" \
-  -e "UNITY_USERNAME=quannlm@vng.com.vn" \
-  -e "UNITY_PASSWORD=nRmlmq.2o01/,unity" \
+  -e "UNITY_USERNAME=$1" \
+  -e "UNITY_PASSWORD=$2" \
   -e "UNITY_DIR=/project" \
   unityci/editor:2020.3.6f1-android-1.0.1 \
   bash
